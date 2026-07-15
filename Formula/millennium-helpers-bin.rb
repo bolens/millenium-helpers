19997cb79e4e3675cc333e134792f1c0f6c6a9e9
Michael Bolens <1218380+bolens@users.noreply.github.com>
2026-07-14 23:02:06 -0600

    chore(packaging): bump to v3.0.0 (#21)

--- Formula/millennium-helpers-bin.rb
+++ Formula/millennium-helpers-bin.rb
@@ -12,22 +12,22 @@ class MillenniumHelpersBin < Formula
   on_macos do
     on_arm do
       url "https://github.com/bolens/millenium-helpers/releases/download/v3.0.0/millennium-helpers-v3.0.0-darwin-arm64.tar.gz"
-      sha256 "bb3532ec10271709638cca737ae05cbe55673a508923c8a4d2f106e6bbae07c6"
+      sha256 "f437e494dc630f0f739a91fad1bbe083df20b403efe7254af876c8e31075b26c"
     end
     on_intel do
       url "https://github.com/bolens/millenium-helpers/releases/download/v3.0.0/millennium-helpers-v3.0.0-darwin-amd64.tar.gz"
-      sha256 "10a75d831d9648c487a5cc303e615902a9565f9e1689403fc5dd4a9ba90db6e6"
+      sha256 "4cbc14dcebed11c80d607e3ee9ad49be93ccfba0e41af3651764d3ad0300a26d"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/bolens/millenium-helpers/releases/download/v3.0.0/millennium-helpers-v3.0.0-linux-arm64.tar.gz"
-      sha256 "5067b7592df4c06b20406c0e8da50cad76c541c401c4b6d6df8cee6d90833535"
+      sha256 "9b35d5eccb0adfe43dd01d9b0d5247454b4d71bb81862931c2d57bab481af440"
     end
     on_intel do
       url "https://github.com/bolens/millenium-helpers/releases/download/v3.0.0/millennium-helpers-v3.0.0-linux-amd64.tar.gz"
-      sha256 "96baa9285be191a136aab460ba4e75edc426842333b7df8f719c8de724730ca1"
+      sha256 "de7a0d7ae896dea26e982c45585b2117015e7224b90f754a5a2774578d1d3d92"
     end
   end
 
